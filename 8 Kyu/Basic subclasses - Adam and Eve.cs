603978a56772926d526c8e145dff62676319e761
Alex <158480710+sxpoz@users.noreply.github.com>
2024-02-21 13:58:49 +0300

    Create Basic subclasses - Adam and Eve.cs

---
+++ 8 Kyu/Basic subclasses - Adam and Eve.cs
@@ -0,0 +1,16 @@
+public class God
+{
+  public static Human[] Create() => new Human[] {new Man(), new Woman()};
+}
+public class Human
+{
+  
+}
+public class Man : Human 
+{
+  
+}
+public class Woman : Human 
+{
+  
+}
